151313e1952c1df4020325a7f78b7bbf7ece98aa
Mahesh Kshirsagar <maksh@microsoft.com>
2021-05-21 18:42:23 +0100

    corrected the scope

--- docs/reference/contoso/armTemplates/deploy.ps1
+++ docs/reference/contoso/armTemplates/deploy.ps1
@@ -8,8 +8,7 @@ New-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Owner' -ObjectId $user.Id
 
 New-AzManagementGroupDeployment `
   -Name old-mg `
-  -Location "UK South" `
+  -Location "uksouth" `
   -ManagementGroupId "old-mg" `
   -TemplateUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-1.json" `
-  -TemplateParameterUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-portal-parameters.json" `
-  -WhatIf
+  -TemplateParameterUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-portal-parameters.json"
