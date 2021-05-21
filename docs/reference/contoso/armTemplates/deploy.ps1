fea25ab7683ab5901eb0eb3efed5f622a289c75b
Mahesh Kshirsagar <maksh@microsoft.com>
2021-05-21 19:27:24 +0100

    corrected the mg dep template

--- docs/reference/contoso/armTemplates/deploy.ps1
+++ docs/reference/contoso/armTemplates/deploy.ps1
@@ -7,8 +7,16 @@ $user = Get-AzADUser -UserPrincipalName (Get-AzContext).Account
 New-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Owner' -ObjectId $user.Id
 
 New-AzManagementGroupDeployment `
-  -Name old-mg `
+  -Name old-2-mg `
   -Location "uksouth" `
   -ManagementGroupId "old-mg" `
-  -TemplateUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-1.json" `
+  -TemplateUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg.json" `
+  -TemplateParameterUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-portal-parameters.json" `
+  -WhatIf
+
+  New-AzManagementGroupDeployment `
+  -Name old-2-mg `
+  -Location "uksouth" `
+  -ManagementGroupId "old-mg" `
+  -TemplateUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg.json" `
   -TemplateParameterUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-portal-parameters.json"
