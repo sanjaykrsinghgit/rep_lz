8211667e4579d8f633357b7634e9ac7babd5de1b
Mahesh Kshirsagar <maksh@microsoft.com>
2021-05-24 10:01:53 +0100

    added scope for subs

--- docs/reference/contoso/armTemplates/deploy.ps1
+++ docs/reference/contoso/armTemplates/deploy.ps1
@@ -10,7 +10,7 @@ New-AzManagementGroupDeployment `
   -Name old-2-mg `
   -Location "uksouth" `
   -ManagementGroupId "old-mg" `
-  -TemplateUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-1.json" `
+  -TemplateUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg.json" `
   -TemplateParameterUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-portal-parameters.json" `
   -WhatIf
 
