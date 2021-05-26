602db54a2702d3a8084ea3da7213da36f8d8e5fc
Mahesh Kshirsagar <maksh@microsoft.com>
2021-05-26 18:35:05 +0100

    corrected param file

--- docs/reference/contoso/armTemplates/deploy.ps1
+++ docs/reference/contoso/armTemplates/deploy.ps1
@@ -9,15 +9,15 @@ New-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Owner' -ObjectId $user.Id
 New-AzManagementGroupDeployment `
   -Name old-2-mg `
   -Location "uksouth" `
-  -ManagementGroupId "old-mg" `
+  -ManagementGroupId "old-3-mg" `
   -TemplateUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg.json" `
   -TemplateParameterUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-portal-parameters.json" `
   -WhatIf
 
   New-AzManagementGroupDeployment `
-  -Name old-2-mg `
+  -Name old-4-mg `
   -Location "uksouth" `
-  -ManagementGroupId "old-mg" `
+  -ManagementGroupId "old-4-mg" `
   -TemplateUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg.json" `
   -TemplateParameterUri "https://raw.githubusercontent.com/Mahesh-MSFT/Enterprise-Scale/deploy-at-mg/docs/reference/contoso/armTemplates/es-vwan-mg-portal-parameters.json"
 
